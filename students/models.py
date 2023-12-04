e16a4458e5e4db36af4c59eec45a1e85391fe0af
GARANCINI <clement.garancini@orange.fr>
2023-12-04 18:18:02 -0500

    20231204: Q1 done, Q2 began, to debug

--- students/models.py
+++ students/models.py
@@ -46,13 +46,42 @@ class PerceptronModel(object):
         # Convert to scalar
         dot_product_scalar = nn.as_scalar(dot_product)
         # Create y Node
-        y = nn.Constant(np.array())
+        if dot_product_scalar == 0:
+            y = nn.Constant(np.array(1.0))
+        else:
+            y = nn.Constant(np.array(np.sign(dot_product_scalar)))
+        return nn.as_scalar(y)
 
     def train(self, dataset: PerceptronDataset) -> None:
         """
         Train the perceptron until convergence.
         """
-        "*** TODO: COMPLETE HERE FOR QUESTION 1 ***"
+        DEBUG = False
+        badClassification = True
+        while badClassification:
+            badClassification = False
+            precision = 0
+            for x, y in dataset.iterate_once(1):
+                y_pred = self.get_prediction(x)
+                if DEBUG:
+                    print(
+                        "x is {}, y is {}, y_pred is {}".format(
+                            x, nn.as_scalar(y), y_pred
+                        )
+                    )
+                badClassification = badClassification or (y_pred != nn.as_scalar(y))
+                if y_pred != nn.as_scalar(y):
+                    self.w.update(x, nn.as_scalar(y))
+                    precision += 1
+            if DEBUG:
+                print(badClassification)
+                print(
+                    "An episode has ended, the precision is: {}%".format(
+                        100 * (dataset.x.shape[0] - precision) / dataset.x.shape[0]
+                    )
+                )
+
+        return None
 
 
 class RegressionModel(object):
@@ -64,7 +93,22 @@ class RegressionModel(object):
 
     def __init__(self) -> None:
         # Initialize your model parameters here
-        "*** TODO: COMPLETE HERE FOR QUESTION 2 ***"
+        self.batch_size = 10
+        self.nbParametersLayer1 = 10
+        self.learning_rate = 0.1
+
+        # Model architecture
+        # Hidden layer:
+        ### Weights
+        self.layer1Parameters = nn.Parameter(1, self.nbParametersLayer1)
+        ### Bias
+        self.layer1Bias = nn.Parameter(1, self.nbParametersLayer1)
+
+        # Output layer:
+        ### Weights
+        self.layer2Parameters = nn.Parameter(self.nbParametersLayer1, 1)
+        ### Bias
+        self.layer2Bias = nn.Parameter(1, 1)
 
     def run(self, x: nn.Constant) -> nn.Node:
         """
@@ -75,7 +119,13 @@ class RegressionModel(object):
         Returns:
             A node with shape (batch_size x 1) containing predicted y-values
         """
-        "*** TODO: COMPLETE HERE FOR QUESTION 2 ***"
+        # Forward Pass of the 1st hidden layer
+        a = nn.ReLU(nn.AddBias(nn.Linear(x, self.layer1Parameters), self.layer1Bias))
+
+        # Forward Pass of the output layer
+        y_pred = nn.AddBias(nn.Linear(a, self.layer2Parameters), self.layer2Bias)
+
+        return y_pred
 
     def get_loss(self, x: nn.Constant, y: nn.Constant) -> nn.Node:
         """
@@ -87,13 +137,38 @@ class RegressionModel(object):
                 to be used for training
         Returns: a loss node
         """
-        "*** TODO: COMPLETE HERE FOR QUESTION 2 ***"
+        loss = nn.SquareLoss(x, y)
+        return loss
 
     def train(self, dataset: RegressionDataset) -> None:
         """
         Trains the model.
         """
-        "*** TODO: COMPLETE HERE FOR QUESTION 2 ***"
+        DEBUG = True
+        done = False
+        while not done:
+            for x, y in dataset.iterate_once(self.batch_size):
+                y_pred = self.run(x)
+                loss = self.get_loss(y, y_pred)
+                grad_w1, grad_b1, grad_w2, grad_b2 = nn.gradients(
+                    loss,
+                    [
+                        self.layer1Parameters,
+                        self.layer1Bias,
+                        self.layer2Parameters,
+                        self.layer2Bias,
+                    ],
+                )
+                self.layer1Parameters.update(grad_w1, self.learning_rate)
+                self.layer1Bias.update(grad_b1, self.learning_rate)
+                self.layer2Parameters.update(grad_w2, self.learning_rate)
+                self.layer2Bias.update(grad_b2, self.learning_rate)
+
+            y_pred = self.run(dataset.x)
+            loss = self.get_loss(dataset.y, y_pred)
+            done = loss < 0.2
+            if DEBUG:
+                print("Episode Ended, loss is {}".format(loss))
 
 
 class DigitClassificationModel(object):
